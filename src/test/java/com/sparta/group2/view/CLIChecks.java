94b00adc24dd8e905ef65992931d416d79a52647
Reggie Howard <92373993+Reg-Howard@users.noreply.github.com>
2023-04-11 09:56:51 +0100

    Update CLI Checks File
    Tiny update, just missed an initial nesting for the CLI checks.

--- src/test/java/com/sparta/group2/view/CLIChecks.java
+++ src/test/java/com/sparta/group2/view/CLIChecks.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CLIChecks {
+    @Nested
+    @DisplayName("Command Line Interface validation checks:")
+    public class CLIValidationChecks {
 
-
+    }
 }
