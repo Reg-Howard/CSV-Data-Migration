f57d3a00bfd74ee20322689fbc19f2abcb551ada
HamzaKazi <hamzakaziii1@gmail.com>
2023-04-12 15:26:09 +0100

    added tests for factory controller/cli

--- src/test/java/com/sparta/group2/controller/FactoryControllerChecks.java
+++ src/test/java/com/sparta/group2/controller/FactoryControllerChecks.java
@@ -1,7 +1,6 @@
 package com.sparta.group2.controller;
 
 import org.junit.jupiter.api.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class FactoryControllerChecks {
     @Nested
---
+++ src/test/java/com/sparta/group2/view/CLITest.java
@@ -0,0 +1,17 @@
+package com.sparta.group2.view;
+
+import com.sparta.group2.controller.EmployeeStorageServiceInterface;
+import com.sparta.group2.model.EmployeeDTO;
+import com.sparta.group2.model.storage.EmployeeStorage;
+import com.sparta.group2.model.storage.EmployeeStorageService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class CLITest {
+
+}
